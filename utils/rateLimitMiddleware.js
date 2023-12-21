e5030b8eff253f5be68bac68c564ed405a329c70
Alen Mandic <alenmandic1@gmail.com>
2023-12-21 22:24:02 +0100

    value fix.

--- utils/rateLimitMiddleware.js
+++ utils/rateLimitMiddleware.js
@@ -4,7 +4,7 @@ const rateLimit = require("express-rate-limit")
 
 const globalLimiter = rateLimit({
   windowMs: 18000000, // 5hours
-  max: 215, // 215 requests every 5 hours
+  max: 1015, // 1015 requests every 5 hours
   message: "You are sending too many requests. Try again later.",
   headers: true,
 })
