2bacd372c6adbe3308087fddb833b76abda60969
Alen Mandic <alenmandic1@gmail.com>
2023-12-28 01:32:47 +0100

    minor rate limiter fix and build update

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const config = require("./utils/config.js")
 // initiliaze an express application.
 const app = express()
 
+app.set('trust proxy', 1);
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
